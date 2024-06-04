b7d18dd8efe9f1d016b59cc765cbde6763fcd328
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2024-06-04 16:08:38 -0600

    Añadir servicio de puntuación

--- src/main.js
+++ src/main.js
@@ -4,13 +4,10 @@ config()
 import express from "express"
 import bodyParser from "body-parser"
 import morgan from "morgan"
-import PlayersService from "./services/players-service.js"
 
 const server = express() 
 
 server.use(bodyParser.json())
 server.use(morgan("combined"))
-
-PlayersService.updateScore(10, 50).then((result) => console.log(result))
 
 server.listen(process.env.API_PORT)
--- src/services/scoring-service.js
+++ src/services/scoring-service.js
@@ -1,4 +1,23 @@
+import knexClient from '../shared/database-connection.js'
+
 export default class {
-  static method() {
+  static async getRanking(scoringPeriod) {
+    const RANKING_LENGTH = 20
+
+    const ranking = await knexClient("score")
+      .join("player", "player.player_id", "=", "score.player_id")
+      .where({
+        period: scoringPeriod
+      })
+      .select(
+        "player.name as name",
+        "player.first_surname as first_surname",
+        "player.second_surname as second_surname",
+        "score.points as score"
+      )
+      .orderBy("score.points", "desc")
+      .limit(RANKING_LENGTH)
+
+    return ranking
   }
 }
